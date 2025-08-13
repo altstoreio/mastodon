93509f20386a10b0455bdbe16cfcb8d6323a62cb
Claire <claire.github-309c@sitedethib.com>
2025-08-13 17:47:24 +0200

    Fix indentation, incorrect dates, and needless hook

--- app/services/post_status_service.rb
+++ app/services/post_status_service.rb
@@ -233,7 +233,7 @@ class PostStatusService < BaseService
       application: @options[:application],
       rate_limit: @options[:with_rate_limit],
       quote_approval_policy: @options[:quote_approval_policy],
-      created_at: @created_at.presence || Time.current,
+      created_at: @created_at.presence || Time.now.utc,
     }.compact
   end
 
